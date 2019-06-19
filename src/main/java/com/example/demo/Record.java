db36d95b6c9d89142090875398bab745579ba7e4
Sayali Thorat <sayali.thorat@v2force.com>
2019-06-19 17:51:00 +0530

    changes

--- src/main/java/com/example/demo/Record.java
+++ src/main/java/com/example/demo/Record.java
@@ -1,18 +1,27 @@
 package com.example.demo;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+
 
 
 @Entity
+@Table(name = "product")
 public class Record {
 
   @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @Column(name="id_product")
   private Long id;
   
+  @Column(name="product_name")
   private String name;
 
   protected Record() {}
